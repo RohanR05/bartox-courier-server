077a5223a87b875a66476b11a31bb56e5ed1e627
003-demo <rrriku05@gmail.com>
2026-08-06 00:05:00 +0600

    create user get apig

--- index.js
+++ index.js
@@ -284,6 +284,11 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
     // rider from post api//
     app.post("/riders", async (req, res) => {
       const rider = req.body;
