5e767024cb53328260f664d0857644c8b16f0298
003-demo <rrriku05@gmail.com>
2026-08-06 23:14:45 +0600

    create make admin api

--- index.js
+++ index.js
@@ -289,6 +289,19 @@ async function run() {
       res.send(result);
     });
 
+    app.patch("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const roleInfo = req.body;
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: roleInfo.role,
+        },
+      };
+      const result = await usersCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
     // rider from post api//
     app.post("/riders", async (req, res) => {
       const rider = req.body;
